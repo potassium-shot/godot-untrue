463ddc4b2fd220b864157c3ebf6d8b74c5745f82
toni-zana <tonizana.dev@gmail.com>
2024-01-08 23:59:34 +0100

    add: missing docs

--- addons/untrue/game_root.gd
+++ addons/untrue/game_root.gd
@@ -13,10 +13,19 @@ static var _instance: GameRoot
 ## Useful for putting the UI into a separate [CanvasLayer].
 @export var hud_root: Node = null
 
+## Optionnal animation player to play transition animations when switching level.
+## See [method GameRoot.transition_to_level].
 @export var transition_animation_player: AnimationPlayer = null
 
+## Optionnal loading screen. Will be instanced in the [member GameRoot.hud_root]
+## if the loading of a level takes more than [member GameRoot.loading_screen_show_timeout]
+## seconds.
 @export var loading_screen: PackedScene = null
-@export var loading_screen_show_timeout: float = 2.0
+
+## If loading a level takes more than this amount of time, the loading screen is
+## instanced, provided it is set.
+@export_range(0.0, 1.0, 0.1, "suffix:sec.", "or_greater", "hide_slider")
+var loading_screen_show_timeout: float = 2.0
 
 var _current_level_path_internal: String
 
@@ -84,7 +93,17 @@ func _set_instanced_level(p_new_level: LevelRoot, p_gamemode_kept: bool):
 		if p_gamemode_kept:
 			level_changed_same_gamemode.emit(_instanced_level)
 
-func transition_to_level(p_level: String, p_transition: StringName = ""):
+## Transitions from the current level to another, specified by a load path.
+## If no transition is specified, the loading is instant. Otherwise, it will
+## try to find an animation with the name of [param p_transition] in the
+## [member GameRoot.transition_animation_player], and will play it before loading
+## the new level, and play it back in reverse after.
+## [br]
+## If an animation of that name is not found, it will try to find 2 animations
+## with the name [code]p_transition + "_start"[/code] and
+## [code]p_transition + "_end"[/code], which are respectively played before and
+## after loading the level.
+func transition_to_level(p_level: String, p_transition: StringName = &""):
 	_current_level_path_internal = p_level
 		
 	if Engine.is_editor_hint():
@@ -151,7 +170,7 @@ func _load_level_async(p_path: String) -> PackedScene:
 	ResourceLoader.load_threaded_request(p_path, "PackedScene")
 	
 	var loading_timer = get_tree().create_timer(loading_screen_show_timeout)
-	loading_timer.timeout.connect(show_loading_screen)
+	loading_timer.timeout.connect(_show_loading_screen)
 	
 	while true:
 		var progress: Array = []
@@ -161,9 +180,9 @@ func _load_level_async(p_path: String) -> PackedScene:
 				loading_level_progress_changed.emit(progress)
 			ResourceLoader.THREAD_LOAD_LOADED:
 				if loading_timer:
-					loading_timer.timeout.disconnect(show_loading_screen)
+					loading_timer.timeout.disconnect(_show_loading_screen)
 				
-				hide_loading_screen()
+				_hide_loading_screen()
 				return ResourceLoader.load_threaded_get(p_path) as PackedScene
 			_:
 				assert(false, "Level loading failed")
@@ -172,16 +191,16 @@ func _load_level_async(p_path: String) -> PackedScene:
 		await get_tree().process_frame
 	
 	if loading_timer:
-		loading_timer.timeout.disconnect(show_loading_screen)
+		loading_timer.timeout.disconnect(_show_loading_screen)
 	
-	hide_loading_screen()
+	_hide_loading_screen()
 	return null
 
-func show_loading_screen():
+func _show_loading_screen():
 	if loading_screen:
 		_instanced_loading_screen = loading_screen.instantiate()
 		hud_root.add_child(_instanced_loading_screen)
 
-func hide_loading_screen():
+func _hide_loading_screen():
 	if _instanced_loading_screen:
 		_instanced_loading_screen.queue_free()
